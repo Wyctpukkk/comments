152812bcf09ab38bd19e422716ed4aa38c873d72
Vitaly <poroshenkoe@gmail.com>
2023-03-13 02:54:25 +0300

    fix: likes, add: keydown send form, comments for code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// получаю элементы из DOM дерева
 const inputName = document.querySelector('.form__input_name');
 const inputText = document.querySelector('.form__input_text');
 const inputDate = document.querySelector('.form__actions_date');
@@ -6,24 +7,35 @@ const errorName = document.querySelector('.errorName');
 const errorText = document.querySelector('.errorText');
 const commentsList = document.querySelector('.comments');
 
+// Инициализирую переменные
 let name;
 let text;
+// Получаю дату и время
 let d = new Date();
 const currentTimeString = `${
   d.getHours() < 10 ? `0${d.getHours()}` : d.getHours()
 }:${d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes()}`;
+// Для удобства помещаю текущее время в переменную
 let date = `Сегодня в ${currentTimeString}`;
 
+// Регулярные выражения для валидации Имени и Текста
 const regexpName = /^[а-я]/;
+const regexpText = /^[0-9A-ZА-ЯЁ]+$/i;
 
+// Промежуточный контроль прохождения валидации
 let validateName = false;
 let validateText = false;
 
+// Навешиваю обработчики событий на элементы
 inputName.addEventListener('input', (e) => validatorName(e.target.value));
 inputText.addEventListener('input', (e) => validatorText(e.target.value));
 inputDate.addEventListener('input', (e) => setDate(e.target.value));
 sendComment.addEventListener('click', (e) => createComment(e));
+document.addEventListener('keydown', (e) => {
+  e.code == 'Enter' ? createComment(e) : '';
+});
 
+// Функция валидации Имени
 function validatorName(value) {
   value == ''
     ? ((errorName.innerHTML = 'Не оставляйте поле пустым'),
@@ -33,15 +45,20 @@ function validatorName(value) {
     : ((errorName.innerHTML = 'Используйте кириллицу'), (validateName = false));
 }
 
+// Функция валидации Текста
 function validatorText(value) {
   value == ''
     ? ((errorText.innerHTML = 'Не оставляйте поле пустым'),
       (validateText = false))
-    : value.toLowerCase().match(regexpName)
+    : value.length < 20
+    ? ((errorText.innerHTML = 'Не менее 20 символов'), (validateText = false))
+    : value.toLowerCase().match(regexpText)
     ? ((errorText.innerHTML = ''), (text = value), (validateText = true))
-    : ((errorText.innerHTML = 'Используйте кириллицу'), (validateText = false));
+    : ((errorText.innerHTML = 'Используйте только Латиницу, Кириллицу и Цифры'),
+      (validateText = false));
 }
 
+// Функция установки времени в комментарий
 function setDate(value) {
   let today = getDate();
   let yesterday = getDate(-1);
@@ -54,6 +71,7 @@ function setDate(value) {
   }
 }
 
+// Функция запроса времени и форматирования в удобный формат
 function getDate(count = 0) {
   let d = new Date();
   d.setDate(d.getDate() + count);
@@ -66,24 +84,34 @@ function getDate(count = 0) {
   return year + '-' + month + '-' + day;
 }
 
-function addLike() {
+// Функция получающаяя актуальное количество комментариев, а так же
+// функция навешивает обработчики событий на удалить и лайкнуть, а так же выполняет их
+function actions() {
   btnLike = [...document.querySelectorAll('.comments__btn_like')];
   btnDelete = [...document.querySelectorAll('.comments__btn_delete')];
   btnLike.map((obj, _) => {
-    obj.addEventListener('click', (e) => e.target.classList.toggle('active'));
+    obj.classList.contains('active')
+      ? obj.addEventListener('click', (e) => {
+          e.target.classList.remove('active');
+          actions();
+        })
+      : obj.addEventListener('click', (e) => {
+          e.target.classList.add('active');
+          actions();
+        });
   });
   btnDelete.map((obj, _) => {
     obj.addEventListener('click', (e) =>
       e.target.parentNode.parentNode.parentNode.parentNode.remove()
     );
   });
-  console.log(btnLike);
 }
-addLike();
+actions(); // самовызов функции при инициализации
 
 function createComment(e) {
   e.preventDefault();
 
+  // итоговая валидация
   if (validateName && validateText) {
     let div = document.createElement('div');
     div.innerHTML = `<div class="comments__item">
@@ -100,13 +128,15 @@ function createComment(e) {
       </div>
     </div>`;
     commentsList.append(div);
+    clearInputs();
   } else {
-    console.log('валидация не пройдена');
+    errorText.innerHTML = 'Не оставляйте поле пустым';
+    errorName.innerHTML = 'Не оставляйте поле пустым';
   }
-  addLike();
-  clearInputs();
+  actions();
 }
 
+// Функция очищающаяя поля ввода при добавлении комментария
 function clearInputs() {
   inputName.value = '';
   inputText.value = '';
